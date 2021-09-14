2382f65b8b8514c61ca9a2b17e171a8fa2983a26
alexsincai <alex.sincai@yahoo.co.uk>
2021-09-14 12:31:03 +0300

    bootstrapped

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template
+from os.path import join
 
+app = Flask(__name__)
+
+
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
+app.jinja_env.strip_trailing_newlines = True
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    from os import system
+
+    system("cp ./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js ./static/bootstrap.js")
+    system("npm run build")
+
+    app.run(debug=True)
